ae81cdfb9da4e8bc6c17348cac9d8e84181113a1
Walter Scarborough <wscarbor@tacc.utexas.edu>
2014-01-28 13:11:58 -0600

    Updated default config settings

--- app/scripts/config/agaveSettingsDefault.js
+++ app/scripts/config/agaveSettingsDefault.js
@@ -1,23 +1,13 @@
-
-/*
-    How To Setup agaveSettings.js
-
-    Replace the following empty parameter strings with appropriate Agave
-    authenticated user credentials: authenticatedUser, authenticatedUserPassword.
-
-    If you want to be able to run unit/integration tests, then also set
-    the following with test internal user credentials: testInternalUser, testInternalUserPassword.
-*/
-
 module.exports = {
 
-    // Auth Settings
+    // WSO2 Auth Settings
     clientKey:    '',
     clientSecret: '',
+    hostname: '',
 
-    // API Settings
-    hostname: '129.114.60.211',
+    // VDJ Service Account User
+    serviceAccountKey: '',
+    serviceAccountSecret: '',
+    serviceAccountToken: ''
 
-    // Endpoints
-    authEndpoint: '/token'
 };
