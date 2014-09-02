868d0045770c46cc84713cc9fef1588764d1b64c
Walter Scarborough <wscarbor@tacc.utexas.edu>
2014-09-02 11:11:43 -0500

    Better path management for start script

--- vdjauth-start.sh
+++ vdjauth-start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # description: Forever for VDJAuth
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 forever stopall
 
-forever -a -l $(pwd)/logs/forever.log -o $(pwd)/logs/stdout.log -e $(pwd)/logs/stderr.log start $(pwd)/app/scripts/app.js
+forever -a -l $DIR/logs/forever.log -o $DIR/logs/stdout.log -e $DIR/logs/stderr.log start $DIR/app/scripts/app.js
