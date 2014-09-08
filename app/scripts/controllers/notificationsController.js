4e7b361278e9bdbdee2311c7118499f0d2879f14
Walter Scarborough <wscarbor@tacc.utexas.edu>
2014-09-08 12:12:27 -0500

    Fixed multiple notification to single client issue

--- app/scripts/controllers/notificationsController.js
+++ app/scripts/controllers/notificationsController.js
@@ -7,14 +7,6 @@ var app = require('../app');
 // Controllers
 var apiResponseController = require('./apiResponseController');
 
-// Models
-var JobArchivePath = require('../models/jobArchivePath');
-var MetadataPermissions = require('../models/metadataPermissions');
-var ServiceAccount = require('../models/serviceAccount');
-
-// Processing
-var agaveIO = require('../vendor/agave/agaveIO');
-
 // Node Libraries
 var Q = require('q');
 
@@ -23,50 +15,6 @@ module.exports = NotificationsController;
 
 NotificationsController.processJobNotifications = function(request, response) {
 
-    // Job Creation - resetting job metadata permissions
-    /*
-        1.  Parse projectUuid out of jobArchivePath
-        2.  Create job metadata associating job w/ projectUuid
-        3.  Get project permissions
-        4.  Update permissions on job metadata
-        5a. Success
-        5b. Fail
-    */
-/*
-    var jobUuid = request.body.jobUuid;
-    var jobArchivePath = new JobArchivePath(request.body.jobArchivePath);
-    var projectUuid = jobArchivePath.getProjectUuid(); // 1.
-
-    var serviceAccount = new ServiceAccount();
-    var jobMetadataUuid;
-
-    agaveIO.createJobMetadata(projectUuid, jobUuid) // 2.
-        .then(function(jobMetadata) {
-            jobMetadataUuid = jobMetadata.uuid;
-
-            return agaveIO.getMetadataPermissions(serviceAccount.accessToken, projectUuid); // 3.
-        })
-        // Apply project pems to new metadata
-        .then(function(projectPermissions) {
-
-            var metadataPermissions = new MetadataPermissions();
-
-            var projectUsernames = metadataPermissions.getUsernamesFromMetadataResponse(projectPermissions);
-
-            var promises = [];
-            for (var i = 0; i < projectUsernames.length; i++) {
-                promises.push(agaveIO.addUsernameToMetadataPermissions(projectUsernames[i], serviceAccount.accessToken, jobMetadataUuid));
-            }
-
-            return Q.all(promises); // 4.
-        })
-        .then(function() {
-            apiResponseController.sendSuccess('Notification processed successfully.', response); // 5a.
-        })
-        .fail(function(error) {
-            apiResponseController.sendError(error.message, response); // 5b.
-        });
-*/
     var jobId   = request.params.jobId;
     var jobUuid = request.query.uuid;
     var jobEvent  = request.query.event;
@@ -84,7 +32,7 @@ NotificationsController.processJobNotifications = function(request, response) {
     console.log("jobError " + jobError);
 
     app.emit(
-        'event:jobNotification',
+        'jobNotification',
         {
             jobId: jobId,
             jobEvent: jobEvent,
--- app/scripts/controllers/websocketController.js
+++ app/scripts/controllers/websocketController.js
@@ -20,6 +20,7 @@ module.exports = WebsocketsController;
 io.on('connection', function(socket) {
     console.log("connection ok. rooms are: " + JSON.stringify(io.sockets.adapter.rooms));
 
+
     socket.on('joinRoom', function(room) {
         console.log("joining room: " + room);
         socket.join(room);
@@ -27,10 +28,14 @@ io.on('connection', function(socket) {
 
     socket.on('disconnect', function() {
         console.log("user disconnected");
+
+        // Clean empty rooms
+        delete io.sockets.adapter.rooms[socket.id];
     });
 
-    app.on('event:jobNotification', function(jobNotification) {
-        io.sockets.in(jobNotification.jobId).emit('jobUpdate:' + jobNotification.jobId, jobNotification);
-        //console.log(io.sockets);
-    });
+});
+
+app.on('jobNotification', function(jobNotification) {
+    console.log("sending jobNotification: " + JSON.stringify(jobNotification));
+    io.sockets.in(jobNotification.jobId).emit('jobUpdate', jobNotification);
 });
