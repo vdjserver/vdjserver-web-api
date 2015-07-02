a638e94301576592942197183fcfe603dbd1d200
Walter Scarborough <wscarbor@tacc.utexas.edu>
2015-07-02 16:07:51 -0500

    Fixed duplicate websocket notification bug

--- app/scripts/controllers/websocketController.js
+++ app/scripts/controllers/websocketController.js
@@ -1,27 +1,37 @@
-
 'use strict';
 
 var app = require('../app');
 
 // Node Libraries
 var io = require('socket.io')(app.server);
+var nsp = io.of('/api/v1');
 
 var WebsocketsController = {};
 module.exports = WebsocketsController;
 
-io.of('/api/v1').on('connection', function(socket) {
+nsp.on('connection', function(socket) {
 
     socket.on('joinRoom', function(room) {
+        //console.log("socket join. Rooms are: " + JSON.stringify(io.sockets.adapter.rooms));
         socket.join(room);
     });
 
     socket.on('disconnect', function() {
+        //console.log("socket disconnect");
+
         // Clean empty rooms
         delete io.sockets.adapter.rooms[socket.id];
     });
-
-    app.on('jobNotification', function(jobNotification) {
-        socket.in(jobNotification.jobId).emit('jobUpdate', jobNotification);
-    });
-
 });
+
+app.on('jobNotification', function(jobNotification) {
+    //console.log("jobNotification received ok: " + JSON.stringify(jobNotification));
+    //console.log("jobNotification recieved. Rooms are: " + JSON.stringify(io.sockets.adapter.rooms));
+    nsp.in(jobNotification.jobId).emit('jobUpdate', jobNotification);
+});
+
+/*
+app.on('fileImportNotification', function(fileImportNotification) {
+    nsp.in(fileImportNotification.projectUuid).emit('fileImportUpdate', fileImportNotification);
+});
+*/
