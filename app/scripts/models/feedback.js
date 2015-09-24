bb81a3a4d86d5baca0cdf01190133ddda9a88607
Walter Scarborough <wscarbor@tacc.utexas.edu>
2015-09-24 14:40:53 -0500

    Fixed serialization bug in feedback model

--- app/scripts/models/feedback.js
+++ app/scripts/models/feedback.js
@@ -7,6 +7,9 @@ var FeedbackUtilities = require('./mixins/feedbackUtilities');
 var Feedback = function(attributes) {
     this.username = attributes.username || '';
     this.feedback = attributes.feedback || '';
+    this.remoteip = attributes.remoteip || '';
+    this.recaptcha_challenge_field = attributes.recaptcha_challenge_field || '';
+    this.recaptcha_response_field = attributes.recaptcha_response_field || '';
 };
 
 _.extend(Feedback.prototype, FeedbackUtilities);
