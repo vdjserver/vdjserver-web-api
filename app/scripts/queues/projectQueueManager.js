a33a92ca37dcc837ffe4b2feaab196e44c66bc7e
Scott Christley <scott.christley@utsouthwestern.edu>
2017-06-07 13:48:40 -0500

    switch add user to project to queue task as it takes a long time

--- app/scripts/queues/projectQueueManager.js
+++ app/scripts/queues/projectQueueManager.js
@@ -421,4 +421,367 @@ ProjectQueueManager.processProjects = function() {
 		done(new Error(msg));
             });
     });
+
+    //
+    // Add user to a project by giving them permissions on all of the project objects.
+    // Set project metadata last so the project doesn't show up in the user's project
+    // list until all other permissions have been set.
+    //
+    // 1. project files and subdirectories
+    // 2. project file metadata
+    // 3. project jobs
+    // 4. project metadata
+    // 5. send emails
+    //
+
+    taskQueue.process('addUsernameToProjectTask', function(task, done) {
+	// 1. project files and subdirectories
+
+        var projectData = task.data;
+	var projectUuid = projectData.projectUuid;
+	var username = projectData.username;
+	var msg;
+
+	console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', start addUsernameToProjectTask: ' + username);
+
+	ServiceAccount.getToken()
+	    .then(function() {
+		/* HOTFIX: Agave bug AH-207 is preventing recursive file permissions from working, so manually recurse the tree
+		// set project file directory + subdirectory permissions recursively
+		.then(function(fileListings) {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - addUsernameToMetadataPermissions for project ' + projectUuid);
+		
+		return agaveIO.addUsernameToFullFilePermissions(username, ServiceAccount.accessToken(), projectUuid, true);
+		}) */
+		// enumerate file list
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - addUsernameToMetadataPermissions for project ' + projectUuid);
+			
+		return agaveIO.enumerateFileListings(projectUuid);
+	    })
+            .then(function(fileListings) {
+		// set permissions
+		//console.log(fileListings);
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - enumerateFileListings for project ' + projectUuid);
+
+		var promises = [];
+
+		function createAgaveCall(username, token, filePath) {
+                    return function() {
+			return agaveIO.setFilePermissions(
+			    token,
+                            username,
+                            'ALL',
+			    false,
+                            filePath
+			);
+                    };
+		}
+
+		for (var i = 0; i < fileListings.length; i++) {
+                    promises[i] = createAgaveCall(
+			username,
+			ServiceAccount.accessToken(),
+			'/projects/' + projectUuid + fileListings[i]
+                    );
+		}
+
+		return promises.reduce(Q.when, new Q());
+		// END HOTFIX
+            })
+            .then(function() {
+                taskQueue
+                    .create('addUsernameToProjectFileMetadataTask', projectData)
+                    .removeOnComplete(true)
+                    .attempts(5)
+                    .backoff({delay: 60 * 1000, type: 'fixed'})
+                    .save()
+                    ;
+            })
+            .then(function() {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', done addUsernameToProjectTask: ' + username);
+		done();
+            })
+            .fail(function(error) {
+		if (!msg) msg = 'VDJ-API ERROR: PermissionsController.addPermissionsForUsername - project ' + projectUuid + ' error ' + error;
+		console.error(msg);
+		webhookIO.postToSlack(msg);
+		done(new Error(msg));
+            });
+    });
+
+    taskQueue.process('addUsernameToProjectFileMetadataTask', function(task, done) {
+	// 2. project file metadata
+
+        var projectData = task.data;
+	var projectUuid = projectData.projectUuid;
+	var username = projectData.username;
+	var msg;
+
+	console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', start addUsernameToProjectFileMetadataTask: ' + username);
+
+	ServiceAccount.getToken()
+	    .then(function() {
+		// get file metadata pems
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - addUsernameToFullFilePermissions for project ' + projectUuid);
+
+		return agaveIO.getProjectFileMetadata(projectUuid);
+            })
+            .then(function(projectFileMetadataPermissions) {
+		// (loop) add to file metadata pems
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - getProjectFileMetadata for project ' + projectUuid);
+
+		var metadata = new MetadataPermissions();
+		var uuids = metadata.getUuidsFromMetadataResponse(projectFileMetadataPermissions);
+
+		var promises = [];
+
+		function createAgaveCall(username, token, metadataUuid) {
+                    return function() {
+			return agaveIO.addUsernameToMetadataPermissions(
+                            username,
+                            token,
+                            metadataUuid
+			);
+                    };
+		}
+
+		for (var i = 0; i < uuids.length; i++) {
+                    promises[i] = createAgaveCall(
+			username,
+			ServiceAccount.accessToken(),
+			uuids[i]
+                    );
+		}
+
+		return promises.reduce(Q.when, new Q());
+            })
+            .then(function() {
+                taskQueue
+                    .create('addUsernameToProjectJobsTask', projectData)
+                    .removeOnComplete(true)
+                    .attempts(5)
+                    .backoff({delay: 60 * 1000, type: 'fixed'})
+                    .save()
+                    ;
+            })
+            .then(function() {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', done addUsernameToProjectFileMetadataTask: ' + username);
+		done();
+            })
+            .fail(function(error) {
+		if (!msg) msg = 'VDJ-API ERROR: PermissionsController.addPermissionsForUsername - project ' + projectUuid + ' error ' + error;
+		console.error(msg);
+		webhookIO.postToSlack(msg);
+		done(new Error(msg));
+            });
+    });
+
+    taskQueue.process('addUsernameToProjectJobsTask', function(task, done) {
+	// 3. project jobs
+
+        var projectData = task.data;
+	var projectUuid = projectData.projectUuid;
+	var username = projectData.username;
+	var msg;
+
+	console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', start addUsernameToProjectJobsTask: ' + username);
+
+	ServiceAccount.getToken()
+	    .then(function() {
+		// get jobs for project
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - addUsernameToMetadataPermissions for project ' + projectUuid);
+
+		//return agaveIO.getJobMetadataForProject(projectUuid);
+		return agaveIO.getJobsForProject(projectUuid);
+            })
+            .then(function(jobMetadatas) {
+		// (loop) add to job permissions
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - getJobsForProject for project ' + projectUuid);
+
+		var metadata = new MetadataPermissions();
+		var uuids = metadata.getJobUuidsFromProjectResponse(jobMetadatas);
+
+		var promises = [];
+
+		function createAgaveCall(username, token, uuid) {
+                    return function() {
+			return agaveIO.addUsernameToJobPermissions(
+                            username,
+                            token,
+                            uuid
+			);
+                    };
+		}
+
+		for (var i = 0; i < uuids.length; i++) {
+                    promises[i] = createAgaveCall(
+			username,
+			ServiceAccount.accessToken(),
+			uuids[i]
+                    );
+		}
+
+		return promises.reduce(Q.when, new Q());
+            })
+            .then(function() {
+                taskQueue
+                    .create('addUsernameToProjectMetadataTask', projectData)
+                    .removeOnComplete(true)
+                    .attempts(5)
+                    .backoff({delay: 60 * 1000, type: 'fixed'})
+                    .save()
+                    ;
+            })
+            .then(function() {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', done addUsernameToProjectJobsTask: ' + username);
+		done();
+            })
+            .fail(function(error) {
+		if (!msg) msg = 'VDJ-API ERROR: PermissionsController.addPermissionsForUsername - project ' + projectUuid + ' error ' + error;
+		console.error(msg);
+		webhookIO.postToSlack(msg);
+		done(new Error(msg));
+            });
+    });
+
+    taskQueue.process('addUsernameToProjectMetadataTask', function(task, done) {
+	// 4. project metadata
+
+        var projectData = task.data;
+	var projectUuid = projectData.projectUuid;
+	var username = projectData.username;
+	var msg;
+
+	console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', start addUsernameToProjectMetadataTask: ' + username);
+
+	ServiceAccount.getToken()
+	    .then(function() {
+		// get all project associated metadata
+		// TODO: this technically should be sufficient for all metadata except for the sole project metadata entry
+		// but not currently as many old metadata entries are missing the associationId
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - addUsernameToJobPermissions for project ' + projectUuid);
+
+		return agaveIO.getAllProjectAssociatedMetadata(projectUuid);
+            })
+            .then(function(allMetadatas) {
+		// (loop) add permissions for user
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - getAllProjectAssociatedMetadata for project ' + projectUuid);
+
+		var metadata = new MetadataPermissions();
+		var uuids = metadata.getUuidsFromMetadataResponse(allMetadatas);
+		//console.log(allMetadatas);
+		//console.log(uuids.length);
+		//console.log(uuids);
+
+		var promises = [];
+
+		function createAgaveCall(username, token, uuid) {
+                    return function() {
+			return agaveIO.addUsernameToMetadataPermissions(
+                            username,
+                            token,
+                            uuid
+			);
+                    };
+		}
+
+		for (var i = 0; i < uuids.length; i++) {
+                    promises[i] = createAgaveCall(
+			username,
+			ServiceAccount.accessToken(),
+			uuids[i]
+                    );
+		}
+
+		return promises.reduce(Q.when, new Q());
+            })
+            .then(function() {
+		// Add new username to project metadata pems
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - getMetadataPermissions for project ' + projectUuid);
+
+		return agaveIO.addUsernameToMetadataPermissions(username, ServiceAccount.accessToken(), projectUuid);
+            })
+            .then(function() {
+                taskQueue
+                    .create('addUsernameToProjectSendEmailTask', projectData)
+                    .removeOnComplete(true)
+                    .attempts(5)
+                    .backoff({delay: 60 * 1000, type: 'fixed'})
+                    .save()
+                    ;
+            })
+            .then(function() {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', done addUsernameToProjectMetadataTask: ' + username);
+		done();
+            })
+            .fail(function(error) {
+		if (!msg) msg = 'VDJ-API ERROR: PermissionsController.addPermissionsForUsername - project ' + projectUuid + ' error ' + error;
+		console.error(msg);
+		webhookIO.postToSlack(msg);
+		done(new Error(msg));
+            });
+    });
+
+    taskQueue.process('addUsernameToProjectSendEmailTask', function(task, done) {
+	// 5. send emails
+
+        var projectData = task.data;
+	var projectUuid = projectData.projectUuid;
+	var username = projectData.username;
+	var msg;
+
+	console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', start addUsernameToProjectSendEmailTask: ' + username);
+
+	ServiceAccount.getToken()
+            .then(function() {
+		return agaveIO.getProjectMetadata(ServiceAccount.accessToken(), projectUuid);
+            })
+	    .then(function(projectMetadata) {
+		projectData.projectName = projectMetadata.value.name;
+		console.log(projectData.projectName);
+		return agaveIO.getMetadataPermissions(ServiceAccount.accessToken(), projectUuid);
+	    })
+            .then(function(projectPermissions) {
+		// send emails
+		var metadataPermissions = new MetadataPermissions();
+		var projectUsernames = metadataPermissions.getUsernamesFromMetadataResponse(projectPermissions);
+
+		var promises = projectUsernames.map(function(user) {
+                    return function() {
+			return agaveIO.getUserProfile(user)
+			    .then(function(userProfileList) {
+				if (userProfileList.length == 0) return;
+				if (username == agaveSettings.guestAccountKey) return;
+				var userProfile = userProfileList[0];
+				if (!userProfile.value.disableUserEmail) {
+				    var vdjWebappUrl = agaveSettings.vdjBackbone
+					+ '/project/' + projectUuid;
+				    emailIO.sendGenericEmail(userProfile.value.email,
+							 'VDJServer user added to project',
+							 'VDJServer user "' + username + '" has been added to project "' + projectData.projectName + '".'
+							 + '<br>'
+							 + 'You can view the project with the link below:'
+							 + '<br>'
+							 + '<a href="' + vdjWebappUrl + '">' + vdjWebappUrl + '</a>.'
+							);
+				}
+			    });
+                    };
+		});
+
+		return promises.reduce(Q.when, new Q());
+	    })
+            .then(function() {
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername, project ' + projectUuid + ', done addUsernameToProjectSendEmailTask: ' + username);
+		console.log('VDJ-API INFO: PermissionsController.addPermissionsForUsername - complete for project ' + projectUuid);
+		done();
+            })
+            .fail(function(error) {
+		if (!msg) msg = 'VDJ-API ERROR: PermissionsController.addPermissionsForUsername - project ' + projectUuid + ' error ' + error;
+		console.error(msg);
+		webhookIO.postToSlack(msg);
+		done(new Error(msg));
+            });
+    });
+
 };
