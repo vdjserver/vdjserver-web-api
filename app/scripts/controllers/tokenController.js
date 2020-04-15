6ea7e7143ccf13fe8ac8995bbcad6898dedd7ea5
Scott Christley <scott.christley@utsouthwestern.edu>
2020-04-15 15:31:19 -0500

    add copyright header

--- app/scripts/controllers/tokenController.js
+++ app/scripts/controllers/tokenController.js
@@ -1,6 +1,35 @@
 
 'use strict';
 
+//
+// tokenController.js
+// User authentication and token refresh
+//
+// VDJServer Analysis Portal
+// VDJ Web API service
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+var TokenController = {};
+module.exports = TokenController;
+
 // Controllers
 var apiResponseController = require('./apiResponseController');
 
@@ -10,9 +39,6 @@ var webhookIO = require('../vendor/webhookIO');
 
 // Node Libraries
 var Q = require('q');
-
-var TokenController = {};
-module.exports = TokenController;
 
 // Retrieves a new user token from Agave and returns it to the client
 TokenController.getToken = function(request, response) {
