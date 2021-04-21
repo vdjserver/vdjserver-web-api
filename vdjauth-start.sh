c44535445d26804ebe341769c50fc58b7cd8ac40
Scott Christley <scott.christley@utsouthwestern.edu>
2021-04-21 17:48:19 -0500

    remove old file

--- vdjauth-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# description: Forever for VDJAuth
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-forever stopall
-
-forever -a -l $DIR/logs/forever.log -o $DIR/logs/stdout.log -e $DIR/logs/stderr.log start $DIR/app/scripts/app.js
